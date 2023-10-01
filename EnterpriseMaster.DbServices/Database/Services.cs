50b259125f801f7fef2d8a6d3c0e4f62ecde61a4
kjszywala <kjszywala@protonmail.com>
2023-10-01 14:12:59 +0100

    Add services table, connection string to json file

---
+++ EnterpriseMaster.DbServices/Database/Services.cs
@@ -0,0 +1,9 @@
+﻿namespace EnterpriseMaster.DbServices.Database
+{
+    public class Services : Bases
+    {
+        public string ServiceName { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
