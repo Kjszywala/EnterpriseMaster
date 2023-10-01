9d366632a1b1ec31911f4c038c18f4547889e1e9
kjszywala <kjszywala@protonmail.com>
2023-10-01 14:24:42 +0100

    Change of database string name

--- EnterpriseMaster.WebApi/Program.cs
+++ EnterpriseMaster.WebApi/Program.cs
@@ -16,16 +16,19 @@ namespace EnterpriseMaster.WebApi
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
             builder.Services.AddApiVersioning(options =>
             {
                 options.DefaultApiVersion = new ApiVersion(1, 0);
                 options.AssumeDefaultVersionWhenUnspecified = true;
                 options.ReportApiVersions = true;
             });
+
             builder.Services.AddDbContext<DatabaseContext>(
                 options =>
-                options.UseSqlServer(builder.Configuration.GetConnectionString("EskinsDbContext"))
+                options.UseSqlServer(builder.Configuration.GetConnectionString("EnterpriseMasterDbContext"))
                 );
+
             //===
             var app = builder.Build();
 
