a8cd33e2678dfee1933ab2461e15105ee65be2a6
Kjszywala <kamil.szywala@magicard.com>
2023-10-03 08:21:14 +0100

    Add order statuses model

---
+++ EnterpriseMaster.DbServices/Models/Database/OrderStatuses.cs
@@ -0,0 +1,8 @@
+﻿namespace EnterpriseMaster.DbServices.Models.Database
+{
+	public class OrderStatuses : Bases
+	{
+        public int Status { get; set; }
+        public string? Discription { get; set; }
+    }
+}
