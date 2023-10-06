42955aaf7fee888344876d6f047c94ce92e0d736
kjszywala <kjszywala@protonmail.com>
2023-10-06 10:02:55 +0100

    Add orders and statuses services

---
+++ EnterpriseMaster.DbServices/Services/OrdersServices.cs
@@ -0,0 +1,15 @@
+﻿using EnterpriseMaster.DbServices.Interfaces;
+using EnterpriseMaster.DbServices.Models.Database;
+
+namespace EnterpriseMaster.DbServices.Services
+{
+    public class OrdersServices : 
+        BaseServices<Orders>,
+        IOrdersServices
+    {
+        public OrdersServices()
+        : base("/api/v1.0/Orders")
+        {
+        }
+    }
+}
