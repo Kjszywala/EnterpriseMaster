ceb2a877c7421cf0a316391ea23332e10b288b1c
kjszywala <kjszywala@protonmail.com>
2023-10-06 09:43:49 +0100

    Add customer information service and categories service

---
+++ EnterpriseMaster.DbServices/Services/CategoriesServices.cs
@@ -0,0 +1,15 @@
+﻿using EnterpriseMaster.DbServices.Interfaces;
+using EnterpriseMaster.DbServices.Models.Database;
+
+namespace EnterpriseMaster.DbServices.Services
+{
+    public class CategoriesServices :
+        BaseServices<Categories>,
+        ICategoriesServices
+    {
+        public CategoriesServices()
+           : base("/api/v1.0/Categories")
+        {
+        }
+    }
+}
---
+++ EnterpriseMaster.DbServices/Services/CustomerInformationsServices.cs
@@ -0,0 +1,15 @@
+﻿using EnterpriseMaster.DbServices.Interfaces;
+using EnterpriseMaster.DbServices.Models.Database;
+
+namespace EnterpriseMaster.DbServices.Services
+{
+    internal class CustomerInformationsServices :
+        BaseServices<CustomerInformation>,
+        ICustomerInformationsServices
+    {
+        public CustomerInformationsServices()
+          : base("/api/v1.0/CustomerInformations")
+        {
+        }
+    }
+}
