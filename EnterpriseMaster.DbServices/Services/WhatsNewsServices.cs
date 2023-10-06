f5f7fe883f463431387d7029c5b244118f02061a
kjszywala <kjszywala@protonmail.com>
2023-10-06 10:27:41 +0100

    Add whats new service

---
+++ EnterpriseMaster.DbServices/Services/WhatsNewsServices.cs
@@ -0,0 +1,15 @@
+﻿using EnterpriseMaster.DbServices.Interfaces;
+using EnterpriseMaster.DbServices.Models.Database;
+
+namespace EnterpriseMaster.DbServices.Services
+{
+    public class WhatsNewsServices :
+        BaseServices<WhatsNew>,
+        IWhatsNewsServices
+    {
+        public WhatsNewsServices()
+            : base("/api/v1.0/WhatsNew")
+        {
+        }
+    }
+}
