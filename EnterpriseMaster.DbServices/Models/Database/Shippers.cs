ae65cf957160d489b4745d992d663e0018e15d92
kjszywala <kjszywala@protonmail.com>
2023-10-06 08:35:58 +0100

    Add Shippers model

---
+++ EnterpriseMaster.DbServices/Models/Database/Shippers.cs
@@ -0,0 +1,12 @@
+﻿namespace EnterpriseMaster.DbServices.Models.Database
+{
+    public class Shippers : Bases
+    {
+        public string CompanyName { get; set; }
+        public string ContactName { get; set; }
+        public string? Phone { get; set; }
+        public string? Email { get; set; }
+        public int? ShippingAddressId { get; set; }
+        public ShippingAddresses? ShippersAddress { get; set; }
+    }
+}
