365a01b5f11d15da5602332e04755e8a864e94cb
kjszywala <kjszywala@protonmail.com>
2023-10-17 09:07:02 +0100

    Some corrections to main page

--- EnterpriseMaster/wwwroot/js/site.js
+++ EnterpriseMaster/wwwroot/js/site.js
@@ -2,3 +2,35 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    setTimeout(showSlides, 4000); // Change image every 2 seconds
+}
+function showDropdownServices() {
+    document.getElementById('toggle-1').checked = true;
+}
+
+function hideDropdownServices() {
+    document.getElementById('toggle-1').checked = false;
+}
+function showDropdownSupport() {
+    document.getElementById('toggle-2').checked = true;
+}
+function hideDropdownSupport() {
+    document.getElementById('toggle-2').checked = false;
+}
