0c5a0f7c9ebee7e1b1a910e0fab69874c9abc29c
kjszywala <kjszywala@protonmail.com>
2023-10-24 10:53:28 +0100

    Add login and registration pages, css corrections

--- EnterpriseMaster.DbServices/Models/Database/Users.cs
+++ EnterpriseMaster.DbServices/Models/Database/Users.cs
@@ -2,15 +2,20 @@
 {
     public class Users : Bases
     {
-        public string FirstName { get; set; }
-        public string SecondName { get; set; }
+        public string? Title { get; set; }
+        public string? FirstName { get; set; }
+        public string? SecondName { get; set; }
         public string Password { get; set; }
         public string? CompanyName { get; set; }
+        public string Email { get; set; }
+        public string? Position { get; set; }
+        public string? BusinesArea { get; set; }
         public int? SubscriptionTypeId { get; set; }
-		public SubscriptionTypes? SubscriptionType { get; set; }
-		public byte[]? Image { get; set; }
-		public int? UserAddressId { get; set; }
-        public bool IsEmployee { get; set; }
+        public SubscriptionTypes? SubscriptionType { get; set; }
+        public byte[]? Image { get; set; }
+        public bool? IsEmployee { get; set; }
+        public bool? Newsletter { get; set; }
+        public int? UserAddressId { get; set; }
         public UsersAdresses? UserAddress { get; set; }
     }
 }
