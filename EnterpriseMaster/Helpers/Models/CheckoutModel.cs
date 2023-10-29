ce268dfd5068665f47c4539d44804ea3f7d1a970
kjszywala <kjszywala@protonmail.com>
2023-10-29 11:55:35 +0000

    Add checkout page

---
+++ EnterpriseMaster/Helpers/Models/CheckoutModel.cs
@@ -0,0 +1,15 @@
+﻿namespace EnterpriseMaster.Helpers.Models
+{
+    public class CheckoutModel
+    {
+        public byte[] SubscriptionImage { get; set; }
+        public string SubscriptionName { get; set; }
+        public string SubscriptionInformation { get; set; }
+        public decimal SubscriptionPrice { get; set; }
+        public decimal SubscriptionVat { get; set; }
+        public decimal SubscriptionPlusVat { get; set; }
+        public decimal SubscriptionDelivery { get; set; }
+        public decimal SubscriptionTotal { get; set; }
+        public byte[] PaymentMethodImage { get; set; }
+    }
+}
